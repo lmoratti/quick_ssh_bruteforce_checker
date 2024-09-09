32eb0aaf882a29de74fc66f914db74dcaa25e651
lizzie moratti <moratti.lizzie@gmail.com>
2024-09-09 15:17:37 -0700

    init

---
+++ check_log.py
@@ -0,0 +1,10 @@
+import re
+
+''' 
+Challenge: write a function that checks a log file to determine 
+which users might have had bruteforce attempts against them. (Specific wording, I do not remember)
+'''
+
+# https://github.com/logpai/loghub/blob/master/OpenSSH/OpenSSH_2k.log
+# Found this online, which looks to be the same sample of 2k
+
